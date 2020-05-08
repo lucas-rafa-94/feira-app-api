ac5cab1fa72e7251bd1aecb2de5fb71b48a064c3
Lucas dos Santos <lucas.rfl.santos.94@gmail.com>
2020-05-08 19:13:45 -0300

    git push

--- src/main/java/com/api/feiraapp/model/CadastroModel.java
+++ src/main/java/com/api/feiraapp/model/CadastroModel.java
@@ -16,9 +16,122 @@ public class CadastroModel {
     private String produtos;
     private String produtorOuRevendedor;
     private String barracas;
-    private String dias;
+
+    private String enderecoSegunda;
+    private String horarioSegunda;
+
+    private String enderecoTerca;
+    private String horarioTerca;
+
+    private String enderecoQuarta;
+    private String horarioQuarta;
+
+    private String enderecoQuinta;
+    private String horarioQuinta;
+
+    private String enderecoSexta;
+    private String horarioSexta;
+
+    private String enderecoSabado;
+    private String horarioSabado;
 
     public CadastroModel() {
+    }
+
+    public String getEnderecoSegunda() {
+        return enderecoSegunda;
+    }
+
+    public void setEnderecoSegunda(String enderecoSegunda) {
+        this.enderecoSegunda = enderecoSegunda;
+    }
+
+    public String getHorarioSegunda() {
+        return horarioSegunda;
+    }
+
+    public void setHorarioSegunda(String horarioSegunda) {
+        this.horarioSegunda = horarioSegunda;
+    }
+
+    public String getEnderecoTerca() {
+        return enderecoTerca;
+    }
+
+    public void setEnderecoTerca(String enderecoTerca) {
+        this.enderecoTerca = enderecoTerca;
+    }
+
+    public String getHorarioTerca() {
+        return horarioTerca;
+    }
+
+    public void setHorarioTerca(String horarioTerca) {
+        this.horarioTerca = horarioTerca;
+    }
+
+    public String getEnderecoQuarta() {
+        return enderecoQuarta;
+    }
+
+    public void setEnderecoQuarta(String enderecoQuarta) {
+        this.enderecoQuarta = enderecoQuarta;
+    }
+
+    public String getHorarioQuarta() {
+        return horarioQuarta;
+    }
+
+    public void setHorarioQuarta(String horarioQuarta) {
+        this.horarioQuarta = horarioQuarta;
+    }
+
+    public String getEnderecoQuinta() {
+        return enderecoQuinta;
+    }
+
+    public void setEnderecoQuinta(String enderecoQuinta) {
+        this.enderecoQuinta = enderecoQuinta;
+    }
+
+    public String getHorarioQuinta() {
+        return horarioQuinta;
+    }
+
+    public void setHorarioQuinta(String horarioQuinta) {
+        this.horarioQuinta = horarioQuinta;
+    }
+
+    public String getEnderecoSexta() {
+        return enderecoSexta;
+    }
+
+    public void setEnderecoSexta(String enderecoSexta) {
+        this.enderecoSexta = enderecoSexta;
+    }
+
+    public String getHorarioSexta() {
+        return horarioSexta;
+    }
+
+    public void setHorarioSexta(String horarioSexta) {
+        this.horarioSexta = horarioSexta;
+    }
+
+    public String getEnderecoSabado() {
+        return enderecoSabado;
+    }
+
+    public void setEnderecoSabado(String enderecoSabado) {
+        this.enderecoSabado = enderecoSabado;
+    }
+
+    public String getHorarioSabado() {
+        return horarioSabado;
+    }
+
+    public void setHorarioSabado(String horarioSabado) {
+        this.horarioSabado = horarioSabado;
     }
 
     public long getId() {
@@ -85,11 +198,4 @@ public class CadastroModel {
         this.barracas = barracas;
     }
 
-    public String getDias() {
-        return dias;
-    }
-
-    public void setDias(String dias) {
-        this.dias = dias;
-    }
 }
